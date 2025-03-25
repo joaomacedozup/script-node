0da34e55955a2fbcf118d087b1c56b0a6c5475ec
João Rafael <joao.macedo@zup.com.br>
2025-03-25 15:04:34 -0300

    teste

---
+++ app/app.js
@@ -0,0 +1,23 @@
+const { getAuthToken } = require('./services/authService');
+
+const main = async () => {
+  try {
+    // Step 1: Get Auth Token
+    const token = await getAuthToken();
+    console.log('Auth Token:', token);
+
+    // // Step 2: Upload a file
+    // const fileUploadResponse = await uploadTemporaryFileToKnowledgeSource(
+    //   token,
+    //   'base_de_conhecimento.md',
+    //   '89cc49-sni-sustentacao-e-monitoria',
+    //   'KNOWLEDGE_SOURCE',
+    //   600
+    // );
+    // console.log('File Upload Response:', fileUploadResponse);
+  } catch (error) {
+    console.error('Error in main process:', error.message);
+  }
+};
+
+main();
