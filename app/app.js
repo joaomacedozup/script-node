4be40ff73fe447cb478143c758d80c5076168ce5
João Rafael <joao.macedo@zup.com.br>
2025-03-25 17:13:59 -0300

    resultado do quick command

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 const { getAuthToken } = require('./services/auth');
-const { createQuickCommandExecution } = require('./services/quickCommands');
+const { createQuickCommandExecution, getExecutionStatus } = require('./services/quickCommands');
 const { readPackageJson } = require('./utils/dependencies');
 
 const main = async () => {
@@ -11,17 +11,10 @@ const main = async () => {
     const packageJson = await readPackageJson();
     // https://genai-code-buddy-api.stackspot.com/v1/quick-commands/create-execution/inventario-de-dependencias
     const quickCommand = await createQuickCommandExecution(token, 'inventario-de-dependencias', packageJson)
-
     console.log(quickCommand)
-    // // Step 2: Upload a file
-    // const fileUploadResponse = await uploadTemporaryFileToKnowledgeSource(
-    //   token,
-    //   'base_de_conhecimento.md',
-    //   '89cc49-sni-sustentacao-e-monitoria',
-    //   'KNOWLEDGE_SOURCE',
-    //   600
-    // );
-    // console.log('File Upload Response:', fileUploadResponse);
+    const quickCommandStatus = await getExecutionStatus(token, quickCommand)
+    // getExecutionStatus
+    console.log(quickCommandStatus)
   } catch (error) {
     console.error('Error in main process:', error.message);
   }
--- app/services/quickCommands.js
+++ app/services/quickCommands.js
@@ -22,4 +22,39 @@ const createQuickCommandExecution = async (token, quick_command, input_data) => 
   }
 };
 
-module.exports = { createQuickCommandExecution };
+const getExecutionStatus = async (token, execution_id) => {
+  const maxRetries = 5; // Número máximo de tentativas
+  let attempt = 0;
+
+  while (attempt < maxRetries) {
+    try {
+      const response = await axios.get(
+        `https://genai-code-buddy-api.stackspot.com/v1/quick-commands/callback/${execution_id}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          },
+        }
+      );
+
+      return response.data; // Retorna os dados se a requisição for bem-sucedida
+    } catch (error) {
+      attempt++;
+      console.error(
+        `Attempt ${attempt} failed:`,
+        error.response?.data || error.message
+      );
+
+      if (attempt >= maxRetries) {
+        console.error('Max retries reached. Throwing error.');
+        throw error; // Lança o erro após atingir o número máximo de tentativas
+      }
+
+      // Aguarda um tempo antes de tentar novamente (opcional)
+      await new Promise((resolve) => setTimeout(resolve, 1000)); // Aguarda 1 segundo
+    }
+  }
+};
+
+module.exports = { createQuickCommandExecution, getExecutionStatus };
