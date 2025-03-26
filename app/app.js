6067df4a08f79198dd59eaede223c3c2a438da11
João Rafael <joao.macedo@zup.com.br>
2025-03-26 18:09:18 -0300

    rodando o script para inserir no stackspot

--- app/app.js
+++ app/app.js
@@ -12,24 +12,24 @@ const main = async () => {
   const token = await getAuthToken();
   console.log('Auth Token:', token);
 
-  // // const packageJson = await readPackageJson();
+  // const packageJson = await readPackageJson();
 
-  // // const quickCommand = await createQuickCommandExecution(token, 'inventario-de-dependencias', packageJson)
-  // // console.log(quickCommand)
+  // const quickCommand = await createQuickCommandExecution(token, 'inventario-de-dependencias', packageJson)
+  // console.log(quickCommand)
 
-  // // const quickCommandStatus = await getExecutionStatus(token, quickCommand)
-  // // console.log(quickCommandStatus)
+  // const quickCommandStatus = await getExecutionStatus(token, quickCommand)
+  // console.log(quickCommandStatus)
 
-  // const fileUploadResponse = await uploadTemporaryFileToKnowledgeSource(
-  //   token,
-  //   allDependencies,
-  //   process.env.KNOWLEDGE_SOURCE_NAME,
-  //   process.env.KNOWLEDGE_SOURCE_ID,
-  //   'KNOWLEDGE_SOURCE',
-  //   600
-  // );
+  const fileUploadResponse = await uploadTemporaryFileToKnowledgeSource(
+    token,
+    allDependencies,
+    process.env.KNOWLEDGE_SOURCE_NAME,
+    process.env.KNOWLEDGE_SOURCE_ID,
+    'KNOWLEDGE_SOURCE',
+    600
+  );
 
-  // console.log('File Upload Response:', fileUploadResponse);
+  console.log('File Upload Response:', fileUploadResponse);
 };
 
 main();
